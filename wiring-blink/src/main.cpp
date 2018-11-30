02f65734fc71b755dcb3bf16587ffa54869d3e36
Jeremy Poulter <jeremy@bigjungle.net>
2018-11-30 21:43:35 +0000

    Added some serial output

--- wiring-blink/src/main.cpp
+++ wiring-blink/src/main.cpp
@@ -11,16 +11,21 @@
 
 void setup()
 {
+  Serial.begin(115200);
+
   // initialize LED digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop()
 {
+  Serial.println("On");
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(LED_BUILTIN, HIGH);
   // wait for a second
   delay(1000);
+  
+  Serial.println("Off");
   // turn the LED off by making the voltage LOW
   digitalWrite(LED_BUILTIN, LOW);
    // wait for a second
